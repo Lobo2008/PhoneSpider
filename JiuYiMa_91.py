04933be5b5422bac80885a1a03ec0bb72f532fe4
luozonghai <842315999@qq.com>
2018-09-18 09:33:54 +0800

    lalla

--- JiuYiMa_91.py
+++ JiuYiMa_91.py
@@ -35,25 +35,29 @@ class JiuYiMa_91:
 
         self.exists = set([phone.replace('.txt','') for phone in os.listdir(self.path)])#目前已经存在的手机号列表
         self.succeNum = 0#成功的数量，以存储为准
-        self.sleeptime = 5
+        self.sleeptime = 10
+        self.TOKEN_FAILED_REASON = ''
 
     def getToken(self):
         print(' 正在获取token ',end="")
         loginUrl = 'http://api.fxhyd.cn/UserInterface.aspx?action=login&username='+self.name+'&password='+self.password
+        print(loginUrl)
+        getTokenOk = False
         try:
             fr = urllib.request.urlopen(loginUrl)
             data=fr.readline()
             idata=str(data, encoding = "utf-8")
             if idata[:7] == 'success':
                 self.token =  idata[8:]
-            fr.close()
-            return True
+                getTokenOk = True
+            else:
+                self.TOKEN_FAILED_REASON = idata
         except error.HTTPError as e:
 	        print (e.code)
         except error.URLError as e:
 	        print (e.reason)
-        fr.close()
-        return False
+        # fr.close()
+        return True if getTokenOk else False
 
             
     def getPhone(self):
@@ -63,17 +67,20 @@ class JiuYiMa_91:
             num = 0
             tmpphones = set()
             while num < self.count:
+                getPhoneOk = False
                 try:
                     fr = urllib.request.urlopen(phoneUrl)
                     data = fr.readline()
                     idata = str(data, encoding = "utf-8")
                     if idata[:7] == 'success':
                         # 获取，源数据格式 success|17121324782
-                        print('    获取到 '+idata[8:],' ',end="")
+                        print('    ',num+1,' :  获取到 '+idata[8:],' ',end="")
                         tmpphones.add(idata[8:])
                         self.releasePhone(idata[8:])#释放
                         self.save2file(idata[8:]) #存储，是否要存，到save2file里面判断
-                    fr.close()
+                        getPhoneOk = True
+                    else:
+                         print('    ',num,' :  获取失败 '+idata,' ',end="")    
                 except error.HTTPError as e:
                     print (e.code)
                 except error.URLError as e:
@@ -81,7 +88,8 @@ class JiuYiMa_91:
                 num += 1
                 time.sleep(self.sleeptime)
             self.phones = list(tmpphones)
-            return True
+            fr.close()
+            return True if getPhoneOk else   False
 
     def save2file(self, phone):
         print(' 正在存储 ',end="")
@@ -106,28 +114,34 @@ class JiuYiMa_91:
         print(' 正在释放 ',end="")
         phoneList = ";".join([phone+'-'+self.ItemId for phone in self.phones])+";"
         releasUrl = 'http://api.fxhyd.cn/UserInterface.aspx?action=release&token='+self.token+'&itemid='+self.ItemId+'&mobile='+phone
-        try:
-            fr = urllib.request.urlopen(releasUrl)
-            fr.close()
-            print(' ok ',end="")
-            return True
-        except error.HTTPError as e:
-            print (e.code)
-        except error.URLError as e:
-            print (e.reason)
-        print(' 释放出错 ',end="")
-        return False
+        releaseOk = False
+        attemps = 0#尝试次数
+        while attemps < 3:
+            try:
+                fr = urllib.request.urlopen(releasUrl)            
+                releaseOk =  True
+            except error.HTTPError as e:
+                print (e.code)
+            except error.URLError as e:
+                print (e.reason)
+            if releaseOk:
+                print(' ok ',end="")
+                break#释放成功，停止循环
+            else:
+                print(' 释放出错,尝试 ',attemps,end="")#释放出错，则再试
+                time.sleep(self.sleeptime*2)
+                fr.close()
+                attemps += 1
+        fr.close()
+        return True if releaseOk else False
+
     def spider(self):
         tokenrs = self.getToken()
-
         if tokenrs:
             print('     token是: ',self.token)
             phoners = self.getPhone()
-            if phoners:
-                print(' 共获取到:',str(len(self.phones))+' 个手机号')
-            else:
-                print(' 手机号获取失败')
+            print(' 共获取到:',str(len(self.phones))+' 个手机号')
         else:
-            print(' 获取token失败（可能原因：账号失效/utf8解码出错）')
+            print(' 获取token失败（可能原因：账号失效/utf8解码出错）',self.TOKEN_FAILED_REASON)
         
 
