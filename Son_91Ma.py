762192bcb7c83a7cb9bd8cadb1318c9e818edf26
luozonghai <842315999@qq.com>
2018-09-20 10:15:59 +0800

    91ma release deal

--- Son_91Ma.py
+++ Son_91Ma.py
@@ -10,7 +10,13 @@ from Base import Base
 
 isLocalTest = True
 
+"""
+官方网站    http://www.91ma.me/
+    获取账号    http://api.fxhyd.cn/UserInterface.aspx?action=getaccountinfo&token=TOKEN
 
+    获取号码    http://api.fxhyd.cn/UserInterface.aspx?action=getmobile&token=TOKEN&itemid=项目编号&excludeno=排除号段
+    释放号码    http://api.fxhyd.cn/UserInterface.aspx?action=release&token=TOKEN&itemid=项目编号&mobile=手机号码
+"""
 class Son_91Ma(Base):
     def __init__(self, name, password):
         self.eventid = '43312' #evenid
@@ -33,20 +39,27 @@ class Son_91Ma(Base):
 
         self.exists = set([phone.replace('.txt','') for phone in os.listdir(self.path)])#目前已经存在的手机号列表
         self.succeNum = 0#成功的数量，以存储为准
-        self.sleeptime = 10
+        self.sleeptime = 5
         self.TOKEN_FAILED_REASON = ''
         self.ENCODING = 'utf-8'
         self.isContinue = True
 
         self.loginUrl = 'http://api.fxhyd.cn/UserInterface.aspx?action=login&username='+self.name+'&password='+self.password
-        self.phoneUrl = 'http://api.fxhyd.cn/UserInterface.aspx?action=getmobile&token='+self.token+'&itemid='+self.ItemId
-        self.releaseUrl = 'http://api.fxhyd.cn/UserInterface.aspx?action=release&token='+self.token+'&itemid='+self.ItemId+'&mobile='
+        self.phoneUrl = 'http://api.fxhyd.cn/UserInterface.aspx?action=getmobile&itemid='+self.ItemId
+        
+        self.releaseUrl_ori = 'http://api.fxhyd.cn/UserInterface.aspx?action=release&itemid='+self.ItemId
+        self.releaseUrl = ""
 
     #token处理器，从接口返回的数据中提取需要的token  success|007214108761d8ac768e9c61085bc3b244f0a1ef
     def tokenResDealer(self, idata):
         if idata[:7] == 'success':
             return idata[8:]
         return False
+
+    #token设置器，获取token以后，还需要将获取电话号码的phoneUrl更新
+    def tokenSetter(self, token):
+        self.token =  token
+        self.phoneUrl = self.phoneUrl+'&token='+self.token
 
     #手机号处理器，从接口返回的数据中提取需要的手机号 succes|13145678888
     def phoneResDealer(self, idata):
@@ -56,13 +69,19 @@ class Son_91Ma(Base):
 
     #释放之前拼接释放链接
     def phoneReleaseDealer(self,phone):
-        self.releaseUrl = self.releaseUrl+phone
+        self.releaseUrl = self.releaseUrl_ori+'&token='+self.token+'&mobile='+phone
+
+    def releaseResDealer(self, idata):
+        # success  释放出错会返回数字 ，比如  2007  表示早就已经被释放了
+        if idata == 'success':
+            return True
+        return False
+
 
     def spider(self):
         tokenrs = self.getToken()
         if tokenrs:
             print('     token是: ',self.token)
-            self.phoneUrl = 'http://api.fxhyd.cn/UserInterface.aspx?action=getmobile&token='+self.token+'&itemid='+self.ItemId
             self.getPhone()
             print(' 共获取到:',str(len(self.phones))+' 个手机号')
         else:
